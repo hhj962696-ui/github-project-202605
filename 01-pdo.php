5478df6aeda50e88b1ca48d5a25885c5caa82c21
mackliu <macklun@gmail.com>
2026-05-18 09:06:07 +0800

    初始化

---
+++ 01-pdo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO連線</title>
+</head>
+<body>
+<h1>PDO連線</h1>    
+<?php
+
+
+?>
+</body>
+</html>
