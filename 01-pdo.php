170e81898ea14e20f4d4ed6e90a839a43648d86f
mackliu <macklun@gmail.com>
2026-05-18 09:48:49 +0800

    練習使用PDO進行CRUD,並在網頁上呈現結果

--- 01-pdo.php
+++ 01-pdo.php
@@ -19,6 +19,44 @@ echo "<pre>";
 print_r($depts);
 echo "</pre>";
 
+$sql_insert="INSERT INTO `dept` (`code`,`name`) VALUES('601','中餐科')";
+//$pdo->exec($sql_insert);
+echo "<h2>新增資料</h2>";
+echo $sql_insert;
+echo "<hr>";
+$depts=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+echo "<pre>";
+print_r($depts);
+echo "</pre>";
+
+
+echo "<h2>更新資料</h2>";
+$sql_update="UPDATE `dept` SET `code`='602' , `name`='西餐科' WHERE `id`='8'";
+$pdo->exec($sql_update);
+echo $sql_update;
+echo "<hr>";
+$depts=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+echo "<pre>";
+print_r($depts);
+echo "</pre>";
+echo "<h2>刪除資料</h2>";
+$sql_delete="DELETE FROM `dept` WHERE `id`='9'";
+$pdo->exec($sql_delete);
+echo $sql_delete;
+echo "<hr>";
+$depts=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+echo "<pre>";
+print_r($depts);
+echo "</pre>";
+
+
 ?>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 </body>
 </html>
